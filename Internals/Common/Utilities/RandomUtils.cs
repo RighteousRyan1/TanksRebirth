f8db88e43ec116565ac64538f71341ed985a4515
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-06 21:02:16 -0300

    Switch to using the Random.Shared static variable

--- Internals/Common/Utilities/RandomUtils.cs
+++ Internals/Common/Utilities/RandomUtils.cs
@@ -5,16 +5,15 @@ namespace TanksRebirth.Internals.Common.Utilities;
 
 public static class RandomUtils
 {
-    public static Random DefaultSeed = new();
     public static float NextFloat(this Random random, float min, float max)
-    => (float)(random.NextDouble() * (max - min) + min);
+    => (float)(Random.Shared.NextDouble() * (max - min) + min);
     public static double NextFloat(this Random random, double min, double max)
-        => random.NextDouble() * (max - min) + min;
+        => Random.Shared.NextDouble() * (max - min) + min;
     public static short Next(this Random random, short min, short max)
         => (short)random.Next(min, max);
     public static byte Next(this Random random, byte min, byte max)
         => (byte)random.Next(min, max);
-    public static T PickRandom<T>(T[] input) => input[DefaultSeed.Next(0, input.Length)];
+    public static T PickRandom<T>(T[] input) => input[Random.Shared.Next(0, input.Length)];
     public static List<T> PickRandom<T>(T[] input, int amount)
     {
         List<T> values = new(amount);
@@ -33,5 +32,5 @@ public static class RandomUtils
         return values;
     }
     public static TEnum PickRandom<TEnum>() where TEnum : struct, Enum
-        => (TEnum)(object)DefaultSeed.Next(0, Enum.GetNames<TEnum>().Length);
+        => (TEnum)(object)Random.Shared.Next(0, Enum.GetNames<TEnum>().Length);
 }
