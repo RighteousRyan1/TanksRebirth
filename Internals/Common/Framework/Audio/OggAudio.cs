3939c7b45e5f08496529832f8753cf8cd77a2d1a
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-06 21:25:47 -0300

    Documentation and some miscellaneous changes.

--- Internals/Common/Framework/Audio/OggAudio.cs
+++ Internals/Common/Framework/Audio/OggAudio.cs
@@ -14,9 +14,12 @@ namespace TanksRebirth.Internals.Common.Framework.Audio
     {
         public void Dispose() {
             GC.SuppressFinalize(this);
+            // Release resources
             Instance.Dispose();
-            _effect.Dispose();
+            _effect.Dispose(); 
             IsDisposed = true;
+            Instance = null;
+            _effect = null;
         }
 
         private SoundEffect _effect;
@@ -26,13 +29,11 @@ namespace TanksRebirth.Internals.Common.Framework.Audio
 
         public string Name { get; set; }
 
-        public OggAudio(string path)
-        {
+        public OggAudio(string path) {
             SongPath = path;
             Load(SongPath);
         }
-        private void Load(string path)
-        {
+        private void Load(string path) {
             var buffer = File.ReadAllBytes(path);
 
             var audioShort = StbVorbis.decode_vorbis_from_memory(buffer, out int sampleRate, out int channels);
@@ -52,20 +53,39 @@ namespace TanksRebirth.Internals.Common.Framework.Audio
             Instance = _effect.CreateInstance();
         }
 
-        public bool IsPaused()
-        {
+        private void ThrowIfDisposed() { // Helper throw method.
+            if (!IsDisposed) return;
+            throw new ObjectDisposedException(Name, "This sound effect instance with the name of {Name} file has been already disposed!");
+        }
+        /// <summary>
+        ///     Verifies if the sound is paused.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">This exception will be thrown if this audio instance has been disposed of.</exception>
+        /// <returns>True if the sound is currently paused. False if it is not.</returns>
+        public bool IsPaused() {
+            ThrowIfDisposed();
             if (_effect == null)
                 return false;
             return Instance.State == SoundState.Paused;
         }
-        public bool IsStopped()
-        {
+        /// <summary>
+        ///     Verifies if the sound has been stopped.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">This exception will be thrown if this audio instance has been disposed of.</exception>
+        /// <returns>True if the sound has stopped playing. False if it is playing.</returns>
+        public bool HasStopped() {
+            ThrowIfDisposed();
             if (_effect == null)
                 return true;
             return Instance.State == SoundState.Stopped;
         }
-        public bool IsPlaying()
-        {
+        /// <summary>
+        ///     Verifies if the sound is currently playing.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">This exception will be thrown if this audio instance has been disposed of.</exception>
+        /// <returns>True if the sound is playing. False if it is not playing.</returns>
+        public bool IsPlaying() {
+            ThrowIfDisposed();
             if (_effect == null)
                 return false;
             return Instance.State == SoundState.Playing;
