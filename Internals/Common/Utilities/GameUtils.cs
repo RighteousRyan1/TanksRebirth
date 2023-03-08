5b6e6e00f7e7663cb37a270a6eabb2be2ad36146
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-07 23:52:42 -0300

    Refactor

--- Internals/Common/Utilities/GameUtils.cs
+++ Internals/Common/Utilities/GameUtils.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace TanksRebirth.Internals.Common.Utilities;
 
@@ -16,30 +17,19 @@ public enum Anchor
 }
 public static class GameUtils
 {
-    public static Vector2 GetAnchor(this Anchor a, Vector2 vector)
-    {
-        switch (a)
-        {
-            case Anchor.TopLeft:
-                return Vector2.Zero;
-            case Anchor.TopRight:
-                return new(vector.X, 0);
-            case Anchor.BottomLeft:
-                return new(0, vector.Y);
-            case Anchor.BottomRight:
-                return new(vector.X, vector.Y);
-            case Anchor.LeftCenter:
-                return new(0, vector.Y / 2);
-                case Anchor.RightCenter:
-                return new(vector.X, vector.Y / 2);
-            case Anchor.Center:
-                return new(vector.X  /2 , vector.Y / 2);
-            case Anchor.TopCenter:
-                return new(vector.X / 2, 0);
-            case Anchor.BottomCenter:
-                return new(vector.X / 2, vector.Y);
-        }
-        return default;
+    public static Vector2 GetAnchor(this Anchor a, Vector2 vector) {
+        return a switch {
+            Anchor.TopLeft => Vector2.Zero,
+            Anchor.TopRight => new(vector.X, 0),
+            Anchor.BottomLeft => new(0, vector.Y),
+            Anchor.BottomRight => new(vector.X, vector.Y),
+            Anchor.LeftCenter => new(0, vector.Y / 2),
+            Anchor.RightCenter => new(vector.X, vector.Y / 2),
+            Anchor.Center => new(vector.X / 2, vector.Y / 2),
+            Anchor.TopCenter => new(vector.X / 2, 0),
+            Anchor.BottomCenter => new(vector.X / 2, vector.Y),
+            _ => throw new ArgumentOutOfRangeException(nameof(a), a, "There is no Anchor set for this Anchor...")
+        };
     }
 
     public static float Distance_WiiTanksUnits(Vector2 position, Vector2 endPoint) => Vector2.Distance(position, endPoint) / 0.7f;
