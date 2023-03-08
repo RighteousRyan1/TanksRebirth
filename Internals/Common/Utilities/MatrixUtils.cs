e969857261f34c408e716ece230cccd9635c9392
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-08 16:39:21 -0300

    Revert "Refactor GetWorldPosition() and GetMouseToWorldRay() to use new method GetWorldRay(Vector2, float) to get the Rays used in their calculations."
    This reverts commit 152ecc86e04f780328b180c39a1b90ac6aa257b2.

--- Internals/Common/Utilities/MatrixUtils.cs
+++ Internals/Common/Utilities/MatrixUtils.cs
@@ -9,14 +9,16 @@ namespace TanksRebirth.Internals.Common.Utilities;
 
 public static class MatrixUtils
 {
-    public static Vector2 ConvertWorldToScreen(Vector3 position, Matrix world, Matrix view, Matrix projection) {
+    public static Vector2 ConvertWorldToScreen(Vector3 position, Matrix world, Matrix view, Matrix projection)
+    {
         var viewport = TankGame.Instance.GraphicsDevice.Viewport;
 
         var proj = viewport.Project(position, projection, view, world);
 
         return new(proj.X, proj.Y);
     }
-    public static Vector3 ConvertScreenToWorld(Vector3 position, Matrix world, Matrix view, Matrix projection) {
+    public static Vector3 ConvertScreenToWorld(Vector3 position, Matrix world, Matrix view, Matrix projection)
+    {
         var viewport = TankGame.Instance.GraphicsDevice.Viewport;
 
         var proj = viewport.Unproject(position, projection, view, world);
@@ -24,31 +26,25 @@ public static class MatrixUtils
         return proj;
     }
 
-    public static Vector3 GetWorldPosition(Vector2 screenCoords, float offset = 0f) {
-        var gamePlane = new Plane(Vector3.UnitY, offset);
+    public static Vector3 GetWorldPosition(Vector2 screenCoords, float offset = 0f)
+    {
+        Plane gamePlane = new(Vector3.UnitY, offset);
 
-        var coordsRay = GetWorldRay(screenCoords);
+        var nearPlane = ConvertScreenToWorld(new Vector3(screenCoords, 0), Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
+        var farPlane = ConvertScreenToWorld(new Vector3(screenCoords, 1), Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
 
-        var distance = coordsRay.Intersects(gamePlane);
+        var mouseRay = new Ray(nearPlane, Vector3.Normalize(farPlane - nearPlane));
+
+        float? distance = mouseRay.Intersects(gamePlane);
 
         if (!distance.HasValue)
             return new();
 
-        return coordsRay.Position + coordsRay.Direction * distance.Value;
+        return mouseRay.Position + mouseRay.Direction * distance.Value;
     }
 
-    public static Ray GetWorldRay(Vector2 xY, float offset = 0f) {
-        var viewport = TankGame.Instance.GraphicsDevice.Viewport;
-
-        var nearPlane = viewport.Unproject(new Vector3(xY, 0), Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
-        var farPlane = viewport.Unproject(new Vector3(xY, 1),Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
-
-        return new Ray(nearPlane, Vector3.Normalize(farPlane - nearPlane));
-    }
-
-    public static Ray GetMouseToWorldRay() {
-        return GetWorldRay(MouseUtils.MousePosition);
-        // The old code is left in case of a regression from this commit.
+    public static Ray GetMouseToWorldRay()
+    {
         var nearPlane = ConvertScreenToWorld(new Vector3(MouseUtils.MousePosition, 0), Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
         var farPlane = ConvertScreenToWorld(new Vector3(MouseUtils.MousePosition, 1), Matrix.Identity, TankGame.GameView, TankGame.GameProjection);
 
