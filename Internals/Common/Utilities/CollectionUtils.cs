7f540ac211973949d477841e84765ae7dde41878
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-07 22:25:04 -0300

    Remove duplicate methods and refactor.

--- Internals/Common/Utilities/CollectionUtils.cs
+++ Internals/Common/Utilities/CollectionUtils.cs
@@ -9,118 +9,50 @@ namespace TanksRebirth.Internals.Common.Utilities;
 
 public static class CollectionUtils
 {
-    public static ICollection<T> Combine<T>(this ICollection<T> collection1, ICollection<T> mergeTo)
-    {
-        foreach (var item in collection1)
+    public static ICollection<T> Combine<T>(this IEnumerable<T> enumerable, ICollection<T> mergeTo) {
+        foreach (var item in enumerable)
             mergeTo.Add(item);
-
         return mergeTo;
     }
-
-    public static TSource TryGetFirst<TSource>(this IEnumerable<TSource> source, out bool found)
-    {
-        if (source == null)
-        {
-            throw new ArgumentNullException(source.ToString());
-        }
-
-        if (source is IList<TSource> list)
-        {
-            if (list.Count > 0)
-            {
-                found = true;
-                return list[0];
-            }
-        }
-        else
-        {
-            using IEnumerator<TSource> e = source.GetEnumerator();
-
-            if (e.MoveNext())
-            {
-                found = true;
-                return e.Current;
-            }
-        }
-
-        found = false;
-        return default!;
-    }
-    public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource value)
-    {
+    public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource value) {
         value = default!;
-        if (source == null)
-        {
-            throw new ArgumentNullException(source.ToString());
-        }
-
-        if (source is IList<TSource> list)
-        {
-            if (list.Count > 0)
-            {
-                value = list[0];
-                return true;
+        switch (source) {
+            case null:
+                throw new ArgumentNullException(nameof(source), "Source IEnumerable can not be null!");
+            case IList<TSource> list: {
+                if (list.Count > 0) {
+                    value = list[0];
+                    return true;
+                }
+                break;
             }
-        }
-        else
-        {
-            using IEnumerator<TSource> e = source.GetEnumerator();
+            default: {
+                using var enumerator = source.GetEnumerator();
 
-            if (e.MoveNext())
-            {
-                value = e.Current;
-                return true;
+                if (enumerator.MoveNext()) {
+                    value = enumerator.Current;
+                    return true;
+                }
+
+                break;
             }
         }
 
         return false;
     }
-
-    public static TSource TryGetFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out bool found)
-    {
-        if (source == null)
-        {
-            throw new ArgumentNullException(source.ToString());
-        }
-
-        if (predicate == null)
-        {
-            throw new ArgumentNullException(predicate.ToString());
-        }
-
-        foreach (TSource element in source)
-        {
-            if (predicate(element))
-            {
-                found = true;
-                return element;
-            }
-        }
-
-        found = false;
-        return default!;
-    }
-    public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out TSource found)
-    {
+    public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out TSource found) {
         found = default!;
 
         if (source == null)
-        {
-            throw new ArgumentNullException(source.ToString());
-        }
+            throw new ArgumentNullException(nameof(source), "Source IEnumerable can not be null!");
 
         if (predicate == null)
-        {
-            throw new ArgumentNullException(predicate.ToString());
-        }
+            throw new ArgumentNullException(nameof(predicate), "The predicate used to find the first element in the source IEnumerable can not be null!");
 
-        foreach (TSource element in source)
-        {
-            if (predicate(element))
-            {
-                found = element;
-                return true;
-            }
+        foreach (var element in source) {
+            if (!predicate(element)) continue;
+            found = element;
+            return true;
         }
 
         return false;
