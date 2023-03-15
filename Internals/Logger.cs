55ccbe0aea27fefbb301f3ecf5942b6e97279a69
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-15 20:14:56 -0300

    Avoid allocating memory by using a StringBuilder to be more efficient when using strings.

--- Internals/Logger.cs
+++ Internals/Logger.cs
@@ -2,21 +2,21 @@
 using System.Diagnostics;
 using System.Reflection;
 using System;
+using System.Text;
 using TanksRebirth.Internals.Common.Utilities;
 
 namespace TanksRebirth.Internals
 {
-    public enum LogType
-    {
+    public enum LogType {
         Info,
         Warn,
         Error,
         Debug
     }
-
+    
     /// <summary>Represents a system which reads and writes to a logging file.</summary>
-    public sealed class Logger : IDisposable
-    {
+    public sealed class Logger : IDisposable {
+        private readonly StringBuilder _stringBuilder = new(128);
         private readonly string writeTo;
 
         public readonly string Name;
@@ -32,7 +32,7 @@ namespace TanksRebirth.Internals
             assembly = Assembly.GetExecutingAssembly();
             Name = name;
 
-            FileName = $"{name}_{DateTime.Now.StringFormatCustom("_")}.log";
+            FileName = _stringBuilder.Append(name).Append('_').Append(DateTime.Now.StringFormatCustom("_")).Append(".log").ToString();
 
             writeTo = Path.Combine(writeFile, $"{FileName}");
 
@@ -51,23 +51,40 @@ namespace TanksRebirth.Internals
         /// <param name="throwException">Whether or not to throw an exception upon write completion.</param>
         /// <exception cref="Exception">If <paramref name="throwException"/> is set to <see langword="true"/>, this exception will be thrown upon write completion.</exception>
         public void Write(object contents, LogType writeType, bool throwException = false) {
+            var contentsAsString = contents.ToString();
             fStream.Position = fStream.Length;
-            string str = $"[{DateTime.Now}] [{assembly.GetName().Name}] [{writeType}]: {contents}";
+            _stringBuilder.Clear(); // Clear the sb to avoid writing stuff we don't really want.
+            // Equivalent to $"[{DateTime.Now}] [{assembly.GetName().Name}] [{writeType}]: {contents}"
+            _stringBuilder
+                .Append('[').Append(DateTime.Now.ToString()).Append("] ")
+                .Append('[').Append(assembly.GetName().Name).Append("] ")
+                .Append('[').Append(FromLogLevel(writeType)).Append("]: ")
+                .Append(contentsAsString);
+
+            var str = _stringBuilder.ToString();
             sWriter.WriteLine(str);
             Debug.WriteLine(str);
             sWriter.Flush();
 
             if (throwException)
-                throw new Exception(contents.ToString());
+                throw new Exception(contentsAsString);
         }
 
+        private static string FromLogLevel(LogType type) { // Converts a LogType to a string without the use of Reflection.
+            return type switch {
+                LogType.Info => "Info",
+                LogType.Warn => "Warn",
+                LogType.Error => "Error",
+                LogType.Debug => "Debug",
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
+        }
+        
         public void Dispose() {
-            sWriter?.Dispose();
-            fStream?.Dispose();
+            sWriter.Dispose();
+            fStream.Dispose();
         }
-
-        ~Logger()
-        {
+        ~Logger() {
             Dispose();
         }
     }
