5913e05da57db724a4d68bbac53adb05020da73e
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2023-03-15 20:36:34 -0300

    Copy refactor from Miscelaneous branch, includes Dispose change.

--- Internals/Common/Framework/Audio/MidiPlayer.cs
+++ Internals/Common/Framework/Audio/MidiPlayer.cs
@@ -11,8 +11,8 @@ namespace TanksRebirth.Internals.Common.Framework.Audio
 {
     public class MidiPlayer : IDisposable
     {
-        private static readonly int sampleRate = 44100;
-        private static readonly int bufferLength = sampleRate / 10;
+        private const int DEFAULT_SAMPLE_RATE = 44100;
+        private const int DEFAULT_BUFFER_LENGTH = DEFAULT_SAMPLE_RATE / 10;
 
         private Synthesizer synthesizer;
         private MidiFileSequencer sequencer;
@@ -20,66 +20,54 @@ namespace TanksRebirth.Internals.Common.Framework.Audio
         private DynamicSoundEffectInstance dynamicSound;
         private byte[] buffer;
 
-        public MidiPlayer(string soundFontPath, SynthesizerSettings settings = null)
-        {
-            if (settings is not null)
-            {
-                settings.SampleRate = sampleRate;
-                synthesizer = new Synthesizer(soundFontPath, settings);
+        public MidiPlayer(string soundFontPath, SynthesizerSettings settings = null) {
+            dynamicSound = new DynamicSoundEffectInstance(DEFAULT_SAMPLE_RATE, AudioChannels.Stereo);
+            buffer = new byte[4 * DEFAULT_BUFFER_LENGTH];
+            dynamicSound.BufferNeeded += (_, _) => SubmitBuffer();
+
+            if (settings is null) {
+                synthesizer = new Synthesizer(soundFontPath, DEFAULT_SAMPLE_RATE); //  Settings were not provided, use the default sample rate.
+                sequencer = new MidiFileSequencer(synthesizer);
+                return;
             }
-            else
-                synthesizer = new Synthesizer(soundFontPath, settings);
+            settings = new(DEFAULT_SAMPLE_RATE);
+            synthesizer = new Synthesizer(soundFontPath, settings);
             sequencer = new MidiFileSequencer(synthesizer);
-
-            dynamicSound = new DynamicSoundEffectInstance(sampleRate, AudioChannels.Stereo);
-            buffer = new byte[4 * bufferLength];
-
-            dynamicSound.BufferNeeded += (s, e) => SubmitBuffer();
         }
 
-        public void Play(MidiFile midiFile, bool loop)
-        {
+        public void Play(MidiFile midiFile, bool loop) {
             sequencer.Play(midiFile, loop);
 
-            if (dynamicSound.State != SoundState.Playing)
-            {
-                SubmitBuffer();
-                dynamicSound.Play();
-            }
+            if (dynamicSound.State == SoundState.Playing) return;
+            SubmitBuffer();
+            dynamicSound.Play();
         }
 
-        public void Stop()
-        {
+        public void Stop() {
             sequencer.Stop();
         }
 
-        private void SubmitBuffer()
-        {
+        private void SubmitBuffer() {
             sequencer.RenderInterleavedInt16(MemoryMarshal.Cast<byte, short>(buffer));
             dynamicSound.SubmitBuffer(buffer, 0, buffer.Length);
         }
 
-        public void Dispose()
-        {
-            if (dynamicSound != null)
-            {
-                dynamicSound.Dispose();
-                dynamicSound = null;
-            }
+        public void Dispose() {
+            if (dynamicSound == null) return;
+            dynamicSound.Dispose();
+            dynamicSound = null;
+            GC.SuppressFinalize(this);
         }
 
-        public void NoteOn(int channel, int key, int velocity)
-        {
+        public void NoteOn(int channel, int key, int velocity) {
             synthesizer.NoteOn(channel, key, velocity);
         }
 
-        public void NoteOff(int channel, int key)
-        {
+        public void NoteOff(int channel, int key) {
             synthesizer.NoteOff(channel, key);
         }
 
-        public void NoteOffAll()
-        {
+        public void NoteOffAll() {
             synthesizer.NoteOffAll(true);
         }
 
