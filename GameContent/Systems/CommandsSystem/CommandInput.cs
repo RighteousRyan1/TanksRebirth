eba26e72e4b636739d2d2d7f2ce7a4d9622cd4cf
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-12-04 01:15:44 -0300

    feat: improve performance by not using the default equality operators.

--- GameContent/Systems/CommandsSystem/CommandInput.cs
+++ GameContent/Systems/CommandsSystem/CommandInput.cs
@@ -3,11 +3,12 @@
 namespace TanksRebirth.GameContent.Systems.CommandsSystem;
 
 /// <summary>A structure which represents the input of a command.</summary>
-public readonly struct CommandInput {
+public readonly struct CommandInput : IEquatable<CommandInput> {
     /// <summary>The name of this <see cref="CommandInput"/>.</summary>
     public readonly string Name;
     /// <summary>The description of this <see cref="CommandInput"/>.</summary>
     public readonly string Description;
+
     /// <summary>Define a command.</summary>
     /// <param name="name">The name of this <see cref="CommandInput"/>.</param>
     /// <param name="description">The description of this <see cref="CommandInput"/>.</param>
@@ -15,6 +16,27 @@ public readonly struct CommandInput {
         Name = name;
         Description = description;
     }
+
     /// <summary>Implicit casting to a tuple containing 2 <see cref="string"/>s.</summary>
     public static implicit operator CommandInput(ValueTuple<string, string> input) => new(input.Item1, input.Item2);
+
+    public bool Equals(CommandInput other) {
+        return Name == other.Name && Description == other.Description;
+    }
+
+    public override bool Equals(object? obj) {
+        return obj is CommandInput other && Equals(other);
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(Name, Description);
+    }
+
+    public static bool operator ==(CommandInput left, CommandInput right) {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(CommandInput left, CommandInput right) {
+        return !left.Equals(right);
+    }
 }
