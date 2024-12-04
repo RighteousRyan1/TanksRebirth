e91ed108063d1f4d3bcb7a8fe9b9bab2db476abb
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-12-04 11:12:56 -0300

    ref: refactor slightly (method is unused, however)

--- Graphics/Cameras/SpectatorCamera.cs
+++ Graphics/Cameras/SpectatorCamera.cs
@@ -6,8 +6,7 @@ using TanksRebirth.Internals.Common.Utilities;
 
 namespace TanksRebirth.Graphics.Cameras;
 
-public class SpectatorCamera : PerspectiveCamera
-{
+public class SpectatorCamera : PerspectiveCamera {
     public SpectatorCamera(float fieldOfView, float aspectRatio, float nearZ, float farZ) : base(fieldOfView, aspectRatio, nearZ, farZ) { }
 
     private Vector3 _cameraTarget;
@@ -19,28 +18,27 @@ public class SpectatorCamera : PerspectiveCamera
     // Camera rotation speed
     public float RotationSpeed = 0.01f;
 
-    public void Update()
-    {
+    public void Update() {
         SetLookAt(Position, _cameraTarget, _cameraUp);
         Projection = Matrix.CreatePerspectiveFieldOfView(FieldOfView, AspectRatio, NearZ, FarZ);
 
         // Update camera orientation
-        if (InputUtils.MouseLeft)
-        {
-            // Calculate mouse delta
-            Vector2 mouseDelta = MouseUtils.MousePosition - _prevMousePos;
+        if (!InputUtils.MouseLeft)
+            return;
 
-            // Calculate camera orientation quaternion
-            Quaternion cameraOrientation = Quaternion.CreateFromYawPitchRoll(
-                -mouseDelta.X * RotationSpeed,
-                -mouseDelta.Y * RotationSpeed,
-                0);
+        // Calculate mouse delta
+        var mouseDelta = MouseUtils.MousePosition - _prevMousePos;
 
-            // Rotate camera up vector
-            _cameraUp = Vector3.Transform(_cameraUp, cameraOrientation);
+        // Calculate camera orientation quaternion
+        var cameraOrientation = Quaternion.CreateFromYawPitchRoll(
+            -mouseDelta.X * RotationSpeed,
+            -mouseDelta.Y * RotationSpeed,
+            0);
 
-            // Update previous mouse position
-            _prevMousePos = MouseUtils.MousePosition;
-        }
+        // Rotate camera up vector
+        _cameraUp = Vector3.Transform(_cameraUp, cameraOrientation);
+
+        // Update previous mouse position
+        _prevMousePos = MouseUtils.MousePosition;
     }
 }
