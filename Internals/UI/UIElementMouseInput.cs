5b707a8aecd1197b13e4b1168d99d2f6196e1cf8
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-12-04 13:02:46 -0300

    fix: traverse the children of UI Elements and correctly add them to the list for input calculations.

--- Internals/UI/UIElementMouseInput.cs
+++ Internals/UI/UIElementMouseInput.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using TanksRebirth.GameContent.Systems;
 using TanksRebirth.Internals.Common;
@@ -15,6 +16,40 @@ namespace TanksRebirth.Internals.UI {
         /// <summary>Whether or not the user is able to interact with this <see cref="UIElement"/>.</summary>
         public bool IsInteractable { get; set; } = true;
 
+        private static List<UIElement> TraverseChildrenFallThroughInputs(UIElement element) {
+            if (!element.FallThroughInputs) return [];
+
+            var nList = new List<UIElement>();
+            var children = element.Children;
+
+            /*
+             *  We must collect all the children of the UIElement which allow to fall through input (and the children of such. Meaning we will have to do some GOOD recursion).
+             */
+
+            foreach (var child in children) {
+                if (child.FallThroughInputs)
+                    nList.AddRange(TraverseChildrenFallThroughInputs(child));
+                else
+                    nList.Add(child);
+            }
+
+            return nList;
+        }
+
+        private static List<UIElement> TraverseFocusedElementsList(List<UIElement> enumerable) {
+            var finalListOfElements_0 = enumerable.ToList(); // Clone
+            foreach (var element in enumerable)
+                finalListOfElements_0.AddRange(TraverseChildrenFallThroughInputs(element));
+
+            // Try to discard duplicates.
+            List<UIElement> finalListOfElements_1 = [];
+
+            foreach (var element in finalListOfElements_0.Where(element => !finalListOfElements_1.Contains(element)))
+                finalListOfElements_1.Add(element);
+
+            return finalListOfElements_1;
+        }
+
         /// <summary>
         /// Gets a <see cref="UIElement"/> at the specified position.
         /// </summary>
@@ -25,35 +60,40 @@ namespace TanksRebirth.Internals.UI {
             List<UIElement> focusedElements = new(AllUIElements.Count / 8);
 
             if (!getHighest) {
-                for (int iterator = AllUIElements.Count; iterator >= 0; iterator--) {
-                    UIElement currentElement = AllUIElements[iterator];
-                    if (!currentElement.IgnoreMouseInteractions && currentElement.IsVisible && currentElement.Hitbox.Contains(position)) {
-                        focusedElements.Add(currentElement);
-                        if (!currentElement.FallThroughInputs)
-                            break;
-                    }
+                for (var iterator = AllUIElements.Count; iterator >= 0; iterator--) {
+                    var currentElement = AllUIElements[iterator];
+                    if (currentElement.IgnoreMouseInteractions || !currentElement.IsVisible || !currentElement.Hitbox.Contains(position))
+                        continue;
+                    if (!currentElement.FallThroughInputs)
+                        break;
+                    focusedElements.Add(currentElement);
                 }
             }
             else {
-                for (int iterator = 0; iterator < AllUIElements.Count; iterator++) {
-                    UIElement currentElement = AllUIElements[iterator];
-                    if (!currentElement.IgnoreMouseInteractions && currentElement.IsVisible && currentElement.Hitbox.Contains(position)) {
+                for (var iterator = 0; iterator < AllUIElements.Count; iterator++) {
+                    var currentElement = AllUIElements[iterator];
+                    if (currentElement.IgnoreMouseInteractions || !currentElement.IsVisible || !currentElement.Hitbox.Contains(position))
+                        continue;
+
+                    if (!focusedElements.Contains(currentElement))
                         focusedElements.Add(currentElement);
-                        if (iterator + 1 <= AllUIElements.Count) {
-                            if (currentElement.FallThroughInputs) {
-                                focusedElements.Add(AllUIElements[iterator + 1]);
-                            }
-                            else {
-                                break;
-                            }
-                        }
-                        //if (!currentElement.FallThroughInputs)
-                        //break;
-                    }
+
+                    if (iterator + 1 >= AllUIElements.Count)
+                        continue;
+
+                    if (!currentElement.FallThroughInputs)
+                        break;
+
+                    focusedElements.Add(AllUIElements[iterator + 1]);
                 }
             }
 
-            return focusedElements;
+            /*
+             *  Assuming this function is EXCLUSIVELY used for inputs... (BECAUSE IT IS)
+             */
+
+            var finalElementsList = TraverseFocusedElementsList(focusedElements);
+            return finalElementsList;
         }
 
         private bool IsInputValid(bool? requiredInput) {
