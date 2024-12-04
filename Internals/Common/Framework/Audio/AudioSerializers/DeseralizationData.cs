c5c43c5ab62d05f8b87e7fc919c710c98dc11ec8
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-12-04 01:15:18 -0300

    feat: split OggAudio from its serializer into an individual class for managing serializing the Vorbis OGG format.

---
+++ Internals/Common/Framework/Audio/AudioSerializers/DeseralizationData.cs
@@ -0,0 +1,13 @@
+﻿namespace TanksRebirth.Internals.Common.Framework.Audio.AudioSerializers;
+
+public struct DeseralizationData {
+    public byte[] binaryData;
+    public int channelCount;
+    public int sampleRate;
+
+    public DeseralizationData(byte[] binaryData, int channelCount, int sampleRate) {
+        this.binaryData = binaryData;
+        this.channelCount = channelCount;
+        this.sampleRate = sampleRate;
+    }
+}
